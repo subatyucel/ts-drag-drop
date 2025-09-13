74df08bd4129109a34022d03cc59bd7fb4fce43a
subat <subat.yucel1@gmail.com>
2025-09-13 13:45:24 +0300

    PRojectItem class created

--- src/app.ts
+++ src/app.ts
@@ -130,6 +130,30 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+
+  get persons() {
+    return this.project.people === 1
+      ? "1 person"
+      : `${this.project.people} persons`;
+  }
+
+  constructor(hostElId: string, project: Project) {
+    super("single-project", hostElId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {}
+  renderContent() {
+    this.newEl.querySelector("h2")!.textContent = this.project.title;
+    this.newEl.querySelector("h3")!.textContent = `${this.persons} assigned.`;
+    this.newEl.querySelector("p")!.textContent = this.project.description;
+  }
+}
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
 
@@ -167,9 +191,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     listEl.innerHTML = "";
 
     for (const prjItem of this.assignedProjects) {
-      const listItem = document.createElement("li");
-      listItem.textContent = prjItem.title;
-      listEl.appendChild(listItem);
+      new ProjectItem(this.newEl.querySelector("ul")!.id, prjItem);
     }
   }
 }
