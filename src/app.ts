7ef94486c4eb4d08c1587db922b161df469beb0f
subat <subat.yucel1@gmail.com>
2025-09-13 11:41:40 +0300

    filter projects implemented and duplicate project item bugfix

--- src/app.ts
+++ src/app.ts
@@ -110,7 +110,11 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === "active") return prj.status === ProjectStatus.Active;
+        return prj.status === ProjectStatus.Finished;
+      });
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
 
@@ -122,6 +126,8 @@ class ProjectList {
     const listEl = document.getElementById(
       `${this.type}-projects-list`
     ) as HTMLUListElement;
+
+    listEl.innerHTML = "";
 
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement("li");
